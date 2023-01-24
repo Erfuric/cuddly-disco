bf79f46bd6142197c2f637e97fe6562877cd2ea8
Bryan S <72003881+momentes@users.noreply.github.com>
2023-01-24 21:03:51 +1100

    Prompt added

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'Name',
+    },
+    {
+      type: 'input',
+      message: 'What is your location?',
+      name: 'Location',
+    },
+    {
+      type: 'input',
+      message: 'Bio?:',
+      name: 'Bio',
+    },
+    {
+        type: 'input',
+        message: 'What is your LinkedIn URL?',
+        name: 'LinkedIn URL',
+     },
+     {
+        type: 'input',
+        message: 'What is your GitHub URL?',
+        name: 'Github URL',
+     },
+  ])
+
